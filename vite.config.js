d75321aec2aa8cfd15258a111f9afce3821c070e
EmilioRosasTr <emilio456cn@gmail.com>
2026-05-06 12:36:11 -0600

    finish

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['nexorfront-production.up.railway.app', '.railway.app'],
+    host: true,
+  },
+  server: {
+    host: true,
+  },
 })
